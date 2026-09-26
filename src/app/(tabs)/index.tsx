3b6d2e79c871e41f0f07a9739b2b7a2dde7916eb
WKWKWK <jeremyarden2808@gmail.com>
2026-09-26 12:16:00 +0700

    Navigation Stack

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,36 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const HomeScreen = () => {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <Text>Hello world</Text>
+      <Link style={{fontSize: 20, color: "blue"}} href={"/about"}>
+        Ke about
+      </Link>
+      <Link style={{fontSize: 16, color: "blue"}} href={"/recipe"}>
+        Ke recipe
+      </Link>
+
+      <Text style={{fontSize: 30, fontWeight: "bold"}}>
+        List Recipe 
+      </Text>
+      <Link style={{fontSize: 20, color: "blue"}} href={"/recipe/1"}>
+        Recipe 1
+      </Link>
+      <Link style={{fontSize: 20, color: "blue"}} href={"/recipe/22"}>
+        Recipe 22
+      </Link>
+      <Link style={{fontSize: 20, color: "blue"}} href={"/recipe/155"}>
+        Recipe 155
+      </Link>
+    </View>
+  )
+}
+
+export default HomeScreen
